b2173cf25f559f699e806c85f7c8a56df04e46ca
Dibbo T <dibbotripura7@gmail.com>
2023-09-30 20:20:51 +0600

    hover has been added

--- index.js
+++
@@ -1,8 +0,0 @@
-const handleColorChange = () => {
-    let a = Math.random() * 258;
-    let b = Math.random() * 258;
-    let c = Math.random() * 258;
-    let color = "rgb(" + a + "," + b + "," + c + ")";
-    console.log(color)
-    document.getElementById("colorflipper").style.backgroundColor = color;
-};
